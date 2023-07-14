6108e5f600a79536ba208ff5bd94f4c3f17c5221
lime <lime@eduroam-5597-73.eduroam.ruhr-uni-bochum.de>
2023-07-14 10:57:08 +0200

    transform image in grayscale, using gauss blur function and canny detection

--- shapedetection.py
+++ shapedetection.py
@@ -1,9 +1,12 @@
 import cv2
-import numpy as np
 
 
 
 image = cv2.imread("source/shapes_v2.png")
 imageContour = image.copy()
+
+image_gray = cv2.cvtColor(image, cv2.COLOR_BGR2GRAY)
+image_blur = cv2.GaussianBlur(image_gray, (7, 7), 1)
+imageCanny = cv2.Canny(image_blur, 50, 50)
 
 
